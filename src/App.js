c13688a78ce6898423650278a19f960260a7f61e
Yuval Tuchman <yuvaltuchman@gmail.com>
2022-05-06 01:37:22 +0300

    navigation bar and logo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 
-import CategoryMenu from "./components/category-menu/category-menu.component";
+import {Routes, Route} from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigatin.component";
+import Shop from "./routes/shop/shop.component";
 const App = ()=> {
-  const categories = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
   return (
-      <CategoryMenu categories = {categories}/>
+      <Routes>
+        <Route path='/' element = {<Navigation/>}>
+            <Route index element = {<Home/>}/>
+            <Route path='shop' element ={<Shop/>}/>
+        </Route>
+      </Routes>
   );
 }
 
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,10 @@
+
+
+const Shop = () =>{
+    return(
+        <div>
+            <h1>I am The shop page</h1>
+        </div>
+    )
+}
+export default Shop;
